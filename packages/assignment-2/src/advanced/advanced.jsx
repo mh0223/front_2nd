eff249798bbffe856bbf6d7dfc869fb6520a64e0
MH <dico659@gmail.com>
2024-06-27 19:12:23 +0900

    ✨ Feat: memo2

--- packages/assignment-2/src/advanced/advanced.jsx
+++ packages/assignment-2/src/advanced/advanced.jsx
@@ -19,7 +19,27 @@ export const memo1 = (fn) => {
   return result;
 };
 
-export const memo2 = (fn) => fn();
+const memo2Cache = new Map();
+export const memo2 = (fn, array) => {
+  //인수 배열을 JSON 문자열로 변환하여 고유한 키로 사용
+  //인수가 [a] 배열이므로 배열의 참조가 다르면 같은 배열 내용이라도 다른 키로 인식
+  //JSON.stringify(args)와 같은 문자열 변환을 통해 키를 만드는 것이 필요
+  const key = JSON.stringify(array);
+
+  //캐시에서 해당 키가 있는지 확인
+  if (memo2Cache.has(key)) {
+    //캐시에 저장된 결과가 있으면 반환
+    return memo2Cache.get(key);
+  }
+
+  //없다면 함수 실행 결과 result에 담기 (바뀐 값 반영) //이렇게 하는게 맞을까요..
+  const result = fn(...array);
+
+  //결과를 캐시에 저장
+  memo2Cache.set(key, result);
+
+  return result;
+};
 
 export const useCustomState = (initValue) => {
   return useState(initValue);
