52dc17f15b4e113a6b67b0b6c0c877fe09409e90
MH <dico659@gmail.com>
2024-06-27 17:49:19 +0900

    ✨ Feat: memo1

--- packages/assignment-2/src/advanced/advanced.jsx
+++ packages/assignment-2/src/advanced/advanced.jsx
@@ -1,13 +1,29 @@
 import { createContext, useContext, useState } from "react";
 
-export const memo1 = (fn) => fn();
+//함수가 사용하는 캐시 객체를 Map 객체를 사용해서 설정
+const memo1Cache = new Map();
+export const memo1 = (fn) => {
+  //캐시 객체에 fn 함수의 결과가 이미 저장되어 있는지 확인
+  if (memo1Cache.has(fn)) {
+    //있다면 캐시에서 fn 함수의 결과를 가져와 반환
+    return memo1Cache.get(fn);
+  }
+
+  //fn 함수 실행하여 결과를 얻음
+  const result = fn();
+
+  //fn 함수 결과를 캐시에 저장
+  memo1Cache.set(fn, result);
+
+  // fn 함수 결과를 반환
+  return result;
+};
 
 export const memo2 = (fn) => fn();
 
-
 export const useCustomState = (initValue) => {
   return useState(initValue);
-}
+};
 
 const textContextDefaultValue = {
   user: null,
@@ -27,36 +43,27 @@ export const TestContextProvider = ({ children }) => {
     <TestContext.Provider value={{ value, setValue }}>
       {children}
     </TestContext.Provider>
-  )
-}
+  );
+};
 
 const useTestContext = () => {
   return useContext(TestContext);
-}
+};
 
 export const useUser = () => {
   const { value, setValue } = useTestContext();
 
-  return [
-    value.user,
-    (user) => setValue({ ...value, user })
-  ];
-}
+  return [value.user, (user) => setValue({ ...value, user })];
+};
 
 export const useCounter = () => {
   const { value, setValue } = useTestContext();
 
-  return [
-    value.count,
-    (count) => setValue({ ...value, count })
-  ];
-}
+  return [value.count, (count) => setValue({ ...value, count })];
+};
 
 export const useTodoItems = () => {
   const { value, setValue } = useTestContext();
 
-  return [
-    value.todoItems,
-    (todoItems) => setValue({ ...value, todoItems })
-  ];
-}
+  return [value.todoItems, (todoItems) => setValue({ ...value, todoItems })];
+};
