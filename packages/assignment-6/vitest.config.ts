b427f23ff5ec4dbe4a9837b278e350a793d501db
MH <dico659@gmail.com>
2024-07-23 01:37:24 +0900

    💚 Fix: vitest config

--- packages/assignment-6/vitest.config.ts
+++ packages/assignment-6/vitest.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [react()],
   test: {
     environment: "jsdom",
+    globals: true,
+    setupFiles: ["./src/setupTests.ts"],
     coverage: {
       provider: "v8",
     },
