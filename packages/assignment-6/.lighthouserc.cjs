0937e7b76011fb03192698a49b3f1c50fbed4d59
MH <dico659@gmail.com>
2024-07-23 12:54:34 +0900

    💚 Feat: lighthouse report & comment
    fix: .lighthouserc.cjs (outputDir: "./lhci_reports")

--- packages/assignment-6/.lighthouserc.cjs
+++ packages/assignment-6/.lighthouserc.cjs
@@ -1,13 +1,12 @@
 module.exports = {
   ci: {
     collect: {
-      staticDistDir: ".",
-      // startServerCommand: "npm run start", // 서버를 키는 명령어를 통해서도 실행 가능
-      url: ["http://localhost:3000"],
-      numberOfRuns: 5,
+      startServerCommand: "npm run start", // 서버를 키는 명령어를 통해서도 실행 가능
+      url: ["http://localhost:5173"],
     },
     upload: {
-      target: "temporary-public-storage",
+      target: "filesystem",
+      outputDir: "./lhci_reports",
     },
   },
 };
