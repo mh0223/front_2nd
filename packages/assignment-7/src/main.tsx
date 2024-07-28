2ec461305fa6a4ea94246c74087aca7fc73fd716
MH <dico659@gmail.com>
2024-07-29 00:24:59 +0900

    🔨 Update: Service Worker setup for API mocking

--- packages/assignment-7/src/main.tsx
+++ packages/assignment-7/src/main.tsx
@@ -1,12 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { ChakraProvider } from '@chakra-ui/react'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+// import { ChakraProvider } from '@chakra-ui/react'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ChakraProvider>
+// ReactDOM.createRoot(document.getElementById("root")!).render(
+//   <React.StrictMode>
+//     <ChakraProvider>
+//       <App />
+//     </ChakraProvider>
+//   </React.StrictMode>
+// );
+
+async function prepare() {
+  const { setupWorker } = await import("msw/browser");
+  const { mockApiHandlers } = await import("./mockApiHandlers.ts");
+  const worker = setupWorker(...mockApiHandlers);
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+
+// Service Worker 등록 = 비동기식
+// prepare 함수는 Service Worker 준비될 때까지 기다림
+// Service Worker가 준비되기 전에 API 요청 발생해서 충돌나는 것 방지
+prepare().then(() => {
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
       <App />
-    </ChakraProvider>
-  </React.StrictMode>,
-)
+    </React.StrictMode>
+  );
+});
