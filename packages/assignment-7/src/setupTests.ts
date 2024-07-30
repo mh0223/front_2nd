2c7770acf2e959acc0be4cc464539cbc8799891e
MH <dico659@gmail.com>
2024-07-30 23:13:00 +0900

    🔧 Update: config files for test

--- packages/assignment-7/src/setupTests.ts
+++ packages/assignment-7/src/setupTests.ts
@@ -1,1 +1,17 @@
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+import { setupServer } from "msw/node";
+import { mockApiHandlers } from "./mockApiHandlers.ts";
+
+const server = setupServer(...mockApiHandlers);
+
+beforeAll(() => server.listen());
+
+afterEach(() => {
+  server.resetHandlers();
+  vi.clearAllMocks();
+});
+
+afterAll(() => {
+  vi.resetAllMocks();
+  server.close();
+});
