220ec869e6e0854cc63ed596e866a4fe170b7201
MH <dico659@gmail.com>
2024-08-22 21:52:46 +0900

    ⚡️ Update: server.tsx using node-cache (advanced)

--- packages/assignment-10/src/advanced/server.tsx
+++ packages/assignment-10/src/advanced/server.tsx
@@ -1,16 +1,29 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import React from 'react';
-import express from 'express';
-import ReactDOMServer from 'react-dom/server';
-import { App } from './App.tsx';
+import React from "react";
+import express from "express";
+import ReactDOMServer from "react-dom/server";
+import { App } from "./App.tsx";
+import NodeCache from "node-cache";
+
+const pageCache = new NodeCache({ stdTTL: 100, checkperiod: 120 });
 
 const app = express();
 const port = 3333;
 
-app.get('*', (req, res) => {
-  const app = ReactDOMServer.renderToString(<App url={req.url}/>);
+app.get("*", (req, res) => {
+  const id = req.url;
+  const cacheKey = `cache_${id}`;
 
+  const cachedPage = pageCache.get(cacheKey);
+
+  if (cachedPage) {
+    return res.send(cachedPage);
+  }
+
+  const app = ReactDOMServer.renderToString(<App url={req.url} />);
+
+  pageCache.set(cacheKey, app);
   res.send(`
     <!DOCTYPE html>
     <html lang="en">
